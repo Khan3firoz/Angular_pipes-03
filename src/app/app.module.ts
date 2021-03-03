59abdb69c73ecdaca49277d14437434d4fdd49c6
Firoz <khan3firoz@gmail.com>
2021-03-03 10:32:15 +0530

    initial commite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomPipePipe } from './custom-pipe.pipe';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomPipePipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule
